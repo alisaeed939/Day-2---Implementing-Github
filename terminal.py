1da7b2b100bf18ff4a0d9d95c684a70d4e0d44b9
alisaeed939 <aliguda31@gmail.com>
2024-07-08 23:16:49 -0700

    First Commit

---
+++ terminal.py
@@ -0,0 +1,18 @@
+#1. Create a repository.
+#2. Create a folder in your local machine.
+#3. Open that folder you have just created in git bash.
+#3. Clone that repository
+#4. Go to in your folder of repository by using "cd (your folder of repository)". That repository will be as a folder in your folder.
+#5. Open it in visual studio code by using "code ."
+#6. Make changes and use the following commands : 
+
+#1. git add (file name)         #Go to staging area
+#2. git commit -m (message)
+#3. git push -u origin (branch name)
+#4. git branch (List all branches)
+#5. git checkout -b (branch name)           Creates branch
+#6. git branch (branch name)                Crates branch
+#7. git checkout (branch name)              You want to move
+#8. git merge (branch name)                 The branch name you want to move but becarefull. Go to your main branch and then use this.
+#9. git push                                Push the commit of merging.
+#10. git status                             Get status of modified , untracking , tracked. Or none.
